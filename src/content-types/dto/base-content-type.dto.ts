e223788e1be34ee1e51eb1cad95d15cdf65dbff7
Der-Alex <alexander.thimm@googlemail.com>
2020-09-14 21:38:43 +0200

    not sure if genius or insanity

--- src/content-types/dto/base-content-type.dto.ts
+++ src/content-types/dto/base-content-type.dto.ts
@@ -1,6 +1,7 @@
 import { IsString } from 'class-validator';
+import { BaseContentTypeInterface } from '../base-content-types/interfaces/base-content-type.interface';
 
-export class BaseContentTypeDto {
+export class BaseContentTypeDto implements BaseContentTypeInterface {
   @IsString()
   name: string;
   @IsString()
