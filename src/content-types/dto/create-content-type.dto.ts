3099fb0f545fe6b6bbf4cda763e238d746e3236a
Der Alex <aw@7pkonzepte.de>
2020-09-15 13:33:57 +0200

    create content types - validate contentTypes not to be an empty array

--- src/content-types/dto/create-content-type.dto.ts
+++ src/content-types/dto/create-content-type.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, ValidateNested } from 'class-validator';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { BaseContentTypeDto } from './base-content-type.dto';
 import { Type } from 'class-transformer';
 import { HtmlBaseContentTypeDto } from './html-base-content-type.dto';
@@ -6,8 +6,9 @@ import { ImageBaseContentTypeDto } from './image-base-content-type.dto';
 import { TextBaseContentTypeDto } from './text-base-content-type.dto';
 
 export class CreateContentTypeDto {
-  @IsArray()
   @ValidateNested({ each: true })
+  @IsArray()
+  @ArrayMinSize(1)
   @Type(() => BaseContentTypeDto, {
     discriminator: {
       property: 'type',
